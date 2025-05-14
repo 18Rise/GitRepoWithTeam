4ff92f7a1d6c2ca148a084101bb842ac2fe7b1e7
Rise The Rizzler <naziehan06@gmail.com>
2025-05-14 22:52:39 +0700

    Create main.c
    pirst

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "calc.h"
+
+int main()
+{
+    int a, b;
+    printf("Masukkan dua angka: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Add: %d\n", add(a, b));
+    printf("Sub: %d\n", sub(a, b));
+    printf("Mul: %d\n", mul(a, b));
+
+    if (b != 0)
+        printf("Div: %.2f\n", divide(a, b));
+    else
+        printf("Div: Error (pembagi nol)\n");
+
+    return 0;
+}
+
+// output\main.exe
